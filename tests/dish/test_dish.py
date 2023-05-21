caddb6753113976c6df864ee7256710630aa167d
FranciscoVieir <franciscovieira6397@gmail.com>
2023-05-21 16:56:29 -0300

    .

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,15 +1,27 @@
-from src.models.dish import Dish
+from src.models.dish import Dish  # noqa: F401, E261, E501
 from src.models.ingredient import Ingredient
 import pytest
 
+
 def test_dish():
-    dish = Dish("Lasanha", 19.99)
+    dish = Dish("Lasagna", 14.80)
 
-    assert dish.name == "Lasanha"
-    assert dish.price == 19.99
+    with pytest.raises(
+        ValueError, match="Dish price must be greater then zero."
+    ):
+        Dish("Lasagna", 0)
 
-    assert dish == Dish("Lasanha", 19.99)
-    assert hash(dish) == hash(Dish("Lasanha", 19.99))
+    with pytest.raises(TypeError, match="Dish price must be float."):
+        Dish("Lasagna", "24")
 
-    with pytest.raises(AssertionError):
-        assert dish.name == "Espaguete"
+    assert dish.name == "Lasagna"
+    assert dish.price == 14.80
+    assert dish == Dish("Lasagna", 14.80)
+    assert hash(dish) == hash("Dish('Lasagna', R$14.80)")
+    assert repr(dish) == "Dish('Lasagna', R$14.80)"
+
+    dish.add_ingredient_dependency(Ingredient("meat"), 1)
+
+    assert dish.get_ingredients() == {Ingredient('meat')}
+    assert not dish.get_restrictions()
+    assert dish.recipe == {Ingredient("meat"): 1}
